4dc3c9db1b4754b62a99896a83da9b525ca864dd
secret114 <llc_1995@163.com>
2016-01-17 18:16:51 +0800

    add some functions

--- src/com/view/AddOrderInfo.java
+++ src/com/view/AddOrderInfo.java
@@ -11,7 +11,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 
 /**
  * Created by llc_1 on 2016/1/17.
@@ -79,18 +83,64 @@ public class AddOrderInfo extends JFrame{
         jLabel3 = new JLabel("课 程 号");
         jLabel4 = new JLabel("教 材 号");
         jLabel5 = new JLabel("订购数量");
-        jComboBox1 = new JComboBox();
-        jComboBox2 = new JComboBox();
-        jComboBox3 = new JComboBox();
-        jComboBox4 = new JComboBox();
+
         /*连接数据库查找相关信息*/
         Daodbc dbc = new Daodbc();
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
         try {
             dbc.getCon();
+            Connection connection = dbc.getConnection();
+
+            String sql1 = "SELECT Cno FROM Classes";
+            String sql2 = "SELECT Tno FROM Teacher";
+            String sql3 = "SELECT Lno FROM Lesson";
+            String sql4 = "SELECT Bno FROM Book";
+
+            Vector vector1 = new Vector();
+            Vector vector2 = new Vector();
+            Vector vector3 = new Vector();
+            Vector vector4 = new Vector();
+            Vector vector5 = new Vector();
+            Vector vector6 = new Vector();
+
+            preparedStatement = connection.prepareStatement(sql1);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                vector1.add(resultSet.getString("Cno"));
+            }
+
+            preparedStatement = connection.prepareStatement(sql2);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                vector2.add(resultSet.getString("Tno"));
+            }
+
+            preparedStatement = connection.prepareStatement(sql2);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                vector3.add(resultSet.getString("Bno"));
+            }
+
+            preparedStatement = connection.prepareStatement(sql2);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                vector4.add(resultSet.getString("Bno"));
+            }
+
+            jComboBox1 = new JComboBox(vector1);
+            jComboBox2 = new JComboBox(vector2);
+            jComboBox3 = new JComboBox(vector3);
+            jComboBox4 = new JComboBox(vector4);
+
+           // jComboBox1.setSize();
+
 
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }finally {
+        dbc.getClose(dbc.getConnection(),null,null);
+    }
 
 //        jComboBox5 = new JComboBox();
 
@@ -100,9 +150,9 @@ public class AddOrderInfo extends JFrame{
 //        jPanel3.setLayout(new GridLayout(1,2));
 //        jPanel4.setLayout(new GridLayout(1,2));
         jPanel1.add(jLabel1);
-        jPanel1.add(jTextField1);
+        jPanel1.add(jComboBox1);
         jPanel2.add(jLabel2);
-        jPanel2.add(passwordField);
+        jPanel2.add(jComboBox2);
 //        jPanel3.add(jLabel3);
 //        jPanel3.add(jTextField3);
 //        jPanel4.add(jLabel4);
@@ -111,18 +161,25 @@ public class AddOrderInfo extends JFrame{
 //        jPanel5.add(jTextField5);
 //        jPanel6.add(jButton1);
 //        jPanel6.add(jButton2);
-        jPanel3.add(jButton1);
-        jPanel3.add(jButton2);
-        this.add(jPanel4);
+        jPanel3.add(jLabel3);
+        jPanel3.add(jComboBox3);
+        jPanel4.add(jLabel4);
+        jPanel4.add(jComboBox4);
+        jPanel5.add(jLabel5);
+        jPanel5.add(jTextField1);
+        jPanel6.add(jButton1);
+        jPanel6.add(jButton2);
+
         this.add(jPanel1);
         this.add(jPanel2);
         this.add(jPanel3);
-//        this.add(jPanel4);
-//        this.add(jPanel5);
+        this.add(jPanel4);
+        this.add(jPanel5);
+        this.add(jPanel6);
 //        this.add(jPanel6);
 
         /*窗体基本设置*/
-        this.setLayout(new GridLayout(4,1));
+        this.setLayout(new GridLayout(6,1));
         this.setTitle("");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
@@ -132,7 +189,7 @@ public class AddOrderInfo extends JFrame{
     }
 
     public static void main(String[] args){
-        AddUsersInfo addUsersInfo = new AddUsersInfo();
+        AddOrderInfo addOrderInfo = new AddOrderInfo();
     }
 
     private JPanel jPanel1;
@@ -162,5 +219,6 @@ public class AddOrderInfo extends JFrame{
     private JComboBox jComboBox2;
     private JComboBox jComboBox3;
     private JComboBox jComboBox4;
-    private JComboBox jComboBox5;
+//    private JComboBox jComboBox;
+
 }
